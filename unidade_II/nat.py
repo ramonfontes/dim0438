1d1693b17383fc70f9d3edbf723eb5dd70edf633
Ramon Fontes <ramonreisfontes@gmail.com>
2026-05-19 07:59:07 -0300

    update repo

---
+++ unidade_II/nat.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+"""
+A: 192.168.254.0/24
+B: 200.90.50.0/24
+
+h1 ---    --- s1 -(A)- r1 --- (B) --- h3
+               |
+h2-------------|
+
+"""
+
+import sys
+
+from mininet.log import setLogLevel, info
+from mn_wifi.cli import CLI
+from mn_wifi.net import Mininet_wifi
+from mininet.term import makeTerm
+
+
+def topology():
+    "Create a network."
+    net = Mininet_wifi()
+
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+    h3 = net.addHost('h3')
+    r1 = net.addHost('r1')
+    s1 = net.addSwitch('s1', failMode='standalone')
+
+    info("*** Configuring nodes\n")
+    net.configureNodes()
+
+    info("*** Associating Stations\n")
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(r1, s1)
+    net.addLink(h3, r1)
+
+    info("*** Starting network\n")
+    net.build()
+    s1.start([])
+
+    r1.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    makeTerm(h1, title='client1', cmd=f"bash -c 'sleep 10 && wget http://200.90.50.1:8000'")
+    makeTerm(h2, title='client2', cmd=f"bash -c 'sleep 10 && wget http://200.90.50.1:8000'")
+    # h3 é um servidor WEB
+    makeTerm(h3, title='web server', cmd=f"bash -c 'python3 -m http.server'")
+    makeTerm(r1, title='nat', cmd=f"bash -c 'watch -n 1 \"conntrack -L -n\"'")
+
+    info("*** Running CLI\n")
+    CLI(net)
+
+    info("*** Stopping network\n")
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topology()
